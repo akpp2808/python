b979442931aa8c321740b46c37579d0cdb42a05f
serg0987 <serg0987@gmail.com>
2011-12-20 13:07:30 -0500

    add1

--- src/redis/pubsub/1.py
+++ src/redis/pubsub/1.py
@@ -2,5 +2,10 @@ import redis
 rc = redis.Redis()
 #print dir(redis)
 
-rc.set('foo','bar')
-print rc.get('foo')
+#rc.set('foo','bar')
+#print rc.get('foo')
+#print dir(rc)
+pubsub = rc.pubsub()
+#print dir(pubsub)
+print pubsub.channels
+
--- src/redis/pubsub/pusher.py
+++ src/redis/pubsub/pusher.py
@@ -3,9 +3,14 @@ Created on Dec 9, 2011
 
 @author: serg
 '''
-import redis, json
+import redis, json, time
 rc = redis.Redis()
-channel = 'ASD1'
-data = {'action':'a'}
-message = json.dumps(data)
-rc.publish(channel, message)
+channel = 'ASD'
+#data = {'action':'a1'}
+message = time.strftime('%s')
+data = {u'action': 'chats:addmessage', u'data': {u'id': u'029b897521cb3c885be0284ef22849ac', u'attr': {u'name': u'Guest_14', u'userid': 14, u'time': u'1323423809', u'message': message, u'unread': 1, u'id': 1250,u'type':'msg'}}}
+msg = json.dumps(data)
+print rc.publish(channel, msg), message
+
+
+#{key: value}
